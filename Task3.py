a4bb80b7706724eda301e408a89171003275d8ef
Kinio621 <44881531+Kinio621@users.noreply.github.com>
2020-04-06 16:47:53 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,21 +1,65 @@
-from fastapi import FastAPI
-
-from pydantic import BaseModel
-
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
-global i
-i=-1
+from pydantic import BaseModel
 
 class Patient(BaseModel):
     name: str
     surename: str
 
-class PatientResp(BaseModel):
+class NumerizedPatient(BaseModel):
     id: int
     patient: Patient
 
-@app.post("/patient",response_model=PatientResp)
-def root(p: Patient):
-    i=i+1
-    return PatientResp(id=i,patient=p)
+global i
+i = -1
+
+patients = []
+
+@app.get('/')
+def hello_world():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+class HelloNameResp(BaseModel):
+    message: str
+
+@app.get('/hello/{name}', response_model=HelloNameResp)
+def hello_name(name: str):
+    return HelloNameResp(message= f"Hello {name}")
+
+@app.get('/method')
+def get_method():
+    return {"method":"GET"}
+
+@app.put('/method')
+def put_method():
+    return {"method":"PUT"}
+
+@app.delete('/method')
+def delete_method():
+    return {"method":"DELETE"}
+
+@app.post('/method')
+def post_method():
+    return {"method":"POST"}
+
+def increment():
+    global i
+    i = i + 1
+
+@app.post('/patient', response_model=NumerizedPatient)
+def post_patient(p: Patient):
+    global i
+    increment()
+    patients.append(p)
+    return NumerizedPatient(id=i, patient = p)
+
+@app.get('/patient/{pk}', response_model=Patient)
+def send_patient(pk: int):
+    global i
+    global patients
+    if (i < pk):
+        raise HTTPException(status_code=204, detail="Item not found")    
+    else:
+        return patients[pk]
